45a7b24c82f128c4daa7f6e7f993e7d445019fb7
jefftermayron <jefftermayron@gmail.com>
2024-02-11 10:04:58 -0300

    learn c#

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿List<string> nomes = new List<string>();
+nomes.Add("Jeffter");
+nomes.Add("Mayron");
+nomes.Add("Ferreira");
+nomes.Add("Olirio");
+
+foreach (var item in nomes)
+{
+    System.Console.WriteLine(item);
+}
